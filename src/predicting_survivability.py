e43c618916fa47c891a263e2c3dd602f303a208d
Leon Henne <leonhenne50@gmail.com>
2022-03-11 15:21:11 +0100

    adding typehints to the code

--- src/predicting_survivability.py
+++ src/predicting_survivability.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from typing import Tuple
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -7,7 +9,7 @@ import mlflow
 
 # Data Exploration Project Code
 
-def load_data():
+def load_data() -> pd.DataFrame:
     '''loading the dataset from a github gist'''
 
     df = pd.read_csv("https://gist.githubusercontent.com/Inwernos/e005f7f0d18d57b256404cff3ce3e690/raw/99ca254b56826910b640ce0719e2fcc89ad1a3a4/titanic.csv")
@@ -21,7 +23,7 @@ def explore_data():
     print(df.info())
     print(df.isnull().sum())
 
-def data_cleaning():
+def data_cleaning() -> pd.DataFrame:
     '''Cleans the data from null values'''
 
     # Deleting the columns "Cabin" due to only 22.9 % non missing values (891 total rows - 678 null values / 891 = 0.2289)
@@ -33,13 +35,13 @@ def data_cleaning():
     
     return df
 
-def feature_preperation():
+def feature_preperation() -> pd.DataFrame:
     '''transforming feature type sex into 0 and 1 instead of female and male'''
 
     df["Sex"] = df["Sex"].map(dict({'male': 1,'female': 0}))
     return df
 
-def splitting_dataset():
+def splitting_dataset() -> pd.DataFrame:
     '''Splitting the data twice into 60% training data, 20% validation data and 20% test data'''
 
     x_train=df.drop('Survived', axis = 1)
@@ -51,7 +53,7 @@ def splitting_dataset():
     test_data.to_csv("data/Titanic_test_dataset.csv", index=False)
     return x_train,x_validate, x_test, y_train,y_validate, y_test
 
-def feature_scaling():
+def feature_scaling() -> pd.DataFrame:
     '''Sklearn StandardScaler scales each feature by substracting the mean and dividing by the standard deviation'''
 
     scaler= StandardScaler()
@@ -62,14 +64,14 @@ def feature_scaling():
 
     return scaled_x_train,scaled_x_validate,scaled_x_test
     
-def train_knn(x_train,y_train,knn_param):
+def train_knn(x_train: pd.DataFrame,y_train: pd.DataFrame,knn_param: int) -> KNeighborsClassifier:
     '''fuctions trains the KNN model with a given hyperparameter of the used next neighbors'''
 
     knn_model=KNeighborsClassifier(n_neighbors=knn_param)
     knn_model.fit(x_train, y_train)
     return knn_model
 
-def validation_evaluate_model(model: KNeighborsClassifier):
+def validation_evaluate_model(model: KNeighborsClassifier) -> Tuple[float,np.ndarray]:
     '''evaluating a model by calculating the metrics on predicted labels'''
 
     # logging the dataset used for evaluating
@@ -77,6 +79,7 @@ def validation_evaluate_model(model: KNeighborsClassifier):
     y_predicted = model.predict(x_validate)
     accuracy = accuracy_score(y_validate,y_predicted)
     matrix = confusion_matrix(y_validate,y_predicted)
+    
     return accuracy, matrix
 
 def hyperparameter_testing():
@@ -84,7 +87,7 @@ def hyperparameter_testing():
 
     for knn_parameter in range(1,len(x_train)+1):
         with mlflow.start_run():
-            
+
             #logging the model parameter
             mlflow.log_param("Random_state", 42)
             mlflow.log_param("Trainset_size", len(x_train))
@@ -107,6 +110,9 @@ def hyperparameter_testing():
             mlflow.log_metric("false_positive", false_positive)
             mlflow.log_metric("false_negative", false_negative)
             mlflow.sklearn.log_model(knn_model, "knn_model")
+
+            # printing out each run with their hyperparameter and accuracy metrik
+            print(str(knn_hyperparameter) +" "+str(accuracy))
 
             mlflow.end_run()
 
